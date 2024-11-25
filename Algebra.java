03d4e7768fc0e2483183f683ecfcebe6e907d1db
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-11-25 17:28:32 +0200

    שאלה 1

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,117 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
-
+        for (int i = 0; i < x2; i++) {
+            x1++;
+        }
+        return x1;
+    }
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+        for (int i = 0; i < x2; i++) {
+            x1--;
+        }
+        return x1;
+    }
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	    int result = 0;
+
+	    boolean isNegative = false; 
+
+	    if (x1 < 0) {
+	        x1 = -x1;
+	        isNegative = !isNegative;
+	    }
+	    if (x1 < 0) {
+	        x1 = -x2;
+	        isNegative = !isNegative;
+	    }
+
+	    for (int i = 0; i < x1; i++) {
+	        result += x2;
+	    }
+
+		if (isNegative) {
+		    return -result;
+		} else {
+		    return result;
+		}
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+        int result = 1;
 
+        for (int i = 0; i < n; i++) {
+        	result = times(result,x);
+        }
+
+        return result;
+    }
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+    	if ( x2 == 0) {
+    		return (000);
+    	}
+	    int result = 0;
+	    boolean isNegative = false; 
+
+	    if (x2 < 0) {
+	        x2 = (-x2);
+	        isNegative = !isNegative;
+	    }
+	    if (x1 < 0) {
+	        x1 = -x1;
+	        isNegative = !isNegative;
+	    }
+	    while (x1 >= x2) {
+	        x1 = minus(x1,x2);
+	        result++; 
+	    }
+	    return result;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+	    if (x2 == 0) {
+    		return (000);
+	    }
+
+	    boolean isNegative = false;
+
+	    if (x1 < 0) {
+	        x1 = -x1;
+	        isNegative = true;
+	    }
+	    if (x2 < 0) {
+	        x2 = -x2;
+	    }
+
+	    while (x1 >= x2) {
+	        x1 = minus(x1, x2);
+	    }
+
+		if (isNegative) {
+		    return (-x1);
+		} else {
+		    return x1;
+		}
+	}
+
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
-}
+    	if (x < 0) {
+    		return (-1);
+    	}
+
+    	int guess = 0;
+
+   		while (times(guess, guess) <= x) {
+       		guess = plus(guess, 1);
+    	}
+    	guess--;
+    	return (guess);
+	}}
